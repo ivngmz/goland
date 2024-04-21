9d0a1d88870095ddb8662428e0423b904169ee00
VanOps <ivan.gomez859@comunidadunir.net>
2024-04-21 13:18:44 +0200

    chore: add datatyoes examples to repo

---
+++ 00intro/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// This function  is main function on the main package,
+// it is obliged to have this function on all go programs
+func main() {
+	/*
+		This ia a
+		multi-
+		line
+		comment
+	*/
+	fmt.Println("Hello, World!")
+}
